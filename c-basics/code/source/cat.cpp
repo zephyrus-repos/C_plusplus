aa5864ccc84adc2a4909cc867339b141a64e8ae5
zhangzhicheng@7950x <zhangzhicheng-7950x>
2026-03-19 13:31:42 +0800

    c basics

---
+++ c-basics/code/source/cat.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	auto buffer = std::string{};
+	std::getline(std::cin, buffer);
+	std::cout << buffer << std::endl;
+
+	return 0;
+}
---
+++ c-basics/code/source/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	int num1 = 5;
+	int num2 = 4;
+	std::cout << (num1 + num2) << "\n";
+
+	return 0;
+}
